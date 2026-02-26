f759bb29712117ffcb32c99381b39d47141b102e
Aidar <zzerch.haruhi@gmail.com>
2026-02-26 19:17:31 +0300

    Тестовое

---
+++ main.py
@@ -0,0 +1,17 @@
+def is_prime(n):
+
+    if not isinstance(n, int) or n < 2:
+        return False
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+
+    i = 3
+    while i * i <= n:
+        if n % i == 0:
+            return False
+        i += 2
+    return True
+
+print(is_prime(303))
